98d8c17a82dc215156406a9945b405e26e0e5bbe
Matthew Presley <matthewpresley@gmail.com>
2024-08-21 10:21:11 +0200

    Added data_fitter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from data_importer import DataImporter
+from data_fitter import DataFitter
+from sqlalchemy import select
 
 
 def create_session(db_engine):
@@ -9,19 +11,25 @@ def create_session(db_engine):
     session = sessionmaker(bind=db_engine)
     session_instance = session()
     return session_instance
-    
 
 def main():
     # Create a SQLite database engine
     db_engine = create_engine('sqlite:///data.db')
     # Create a session
     session_instance = create_session(db_engine)
-    # Create model to copy CSV data to database
+
+    # Create model to move data to database
     data_import = DataImporter(session_instance)
-    # Import training data from CSVs to database
+    # Import data from CSVs to database
     data_import.import_data_from_csv('./dataset/train.csv', 'training_data')
-    data_import.import_data_from_csv('./dataset/test.csv', 'test_data')
     data_import.import_data_from_csv('./dataset/ideal.csv', 'ideal_function')
+
+    # Fit training data to find ideal functions
+    data_fit = DataFitter(session_instance)
+    data_fit.fit_data('training_data', 'ideal_function')
+
+
+
     
 
 if __name__ == "__main__":
